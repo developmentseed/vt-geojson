8064d9fdc873f86429c29d11ae333d173731974a
Anand Thakker <vestibule@anandthakker.net>
2015-04-16 13:52:19 -0400

    use tile source metadata to choose tiles if none are given

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
+var path = require('path')
 var zlib = require('zlib')
 var from = require('from2')
 var Pbf = require('pbf')
 var VectorTile = require('vector-tile').VectorTile
 var JSONStream = require('JSONStream')
-var split = require('split')
-var through = require('through2')
 var tilelive = require('tilelive')
+var cover = require('tile-cover')
+var bboxPoly = require('turf-bbox-polygon')
 require('mbtiles').registerProtocols(tilelive)
 require('tilejson').registerProtocols(tilelive)
 
 module.exports = vectorTilesToGeoJSON
 
 /**
- * Given a Tilelive uri for a vector tile source and an array of [z, x, y] tiles,
- * return a GeoJSON FeatureCollection of all features in those
+ * Given a Tilelive uri for a vector tile source and an array of [z, x, y]
+ * tiles, return a GeoJSON FeatureCollection of all features in those
  * tiles.
  */
 function vectorTilesToGeoJSON (uri, tiles) {
-  tiles = [].concat(tiles)
-
+  tiles = tiles ? [].concat(tiles) : null
   var source = null
 
-  var _read = function (_, next) {
+  return from.obj(function (size, next) {
+    var self = this
+    if (source) {
+      return read.call(self, size, next)
+    } else {
+      tilelive.load(uri, function (err, src) {
+        if (err) { return next(err) }
+        source = src
+        source.getInfo(function (err, info) {
+          if (err) { return next(err) }
+          if (tiles === null) {
+            tiles = cover.tiles(
+              bboxPoly(info.bounds).geometry,
+              { min_zoom: info.minzoom, max_zoom: info.maxzoom }
+            )
+          }
+          read.call(self, size, next)
+        })
+      })
+    }
+  })
+
+  function read (_, next) {
     var self = this
     if (tiles.length === 0) return self.push(null)
 
@@ -35,7 +57,7 @@ function vectorTilesToGeoJSON (uri, tiles) {
       // probably be configurable or something
       if (err) {
         console.error(z, x, y, err)
-        return _read.call(self, _, next)
+        return read.call(self, _, next)
       }
 
       if (opts['Content-Encoding'] === 'gzip') {
@@ -48,64 +70,31 @@ function vectorTilesToGeoJSON (uri, tiles) {
         if (err) { return next(err) }
 
         var vt = new VectorTile(new Pbf(tiledata))
+
         Object.keys(vt.layers)
-          .map(function (ln) {
-            return vt.layers[ln]
-          })
-          .forEach(function (layer) {
+          .forEach(function (ln) {
+            var layer = vt.layers[ln]
             for (var i = 0; i < layer.length; i++) {
               var feat = layer.feature(i).toGeoJSON(x, y, z)
               self.push(feat)
             }
           })
-        _read.call(self, _, next)
+        read.call(self, _, next)
       }
     })
   }
-
-  return from.obj(function read (size, next) {
-    var self = this
-    if (source) {
-      return _read.call(self, size, next)
-    } else {
-      tilelive.load(uri, function (err, src) {
-        if (err) { return next(err) }
-        source = src
-        return _read.call(self, size, next)
-      })
-    }
-  })
 }
 
 if (require.main === module) {
   var uri = process.argv[2]
+  if (!/^\w*\:\/\//.test(uri)) {
+    uri = 'mbtiles://' + path.resolve(uri)
+  }
 
   var json = JSONStream.stringify('{ "type": "FeatureCollection", "features": [ ',
     '\n,\n', '] }')
   json.pipe(process.stdout)
 
-  if (process.argv.length > 3) {
-    var tile = process.argv.slice(3, 6).map(Number)
-    vectorTilesToGeoJSON(uri, [tile])
-      .pipe(json)
-  } else {
-    // TODO: this is a bogus way to do the duplex stream, but
-    // shouldn't matter since we're only buffering a list of tiles, which
-    // is WAY smaller than the tiles themselves
-    var tiles = []
-    process.stdin
-      .pipe(split())
-      .pipe(through(function (line, enc, next) {
-        var t = line.toString().split(' ').map(Number)
-        if (t.length === 3) {
-          tiles.push(t)
-        }
-        next()
-      }))
-      .on('data', function () {})
-      .on('end', function () {
-        vectorTilesToGeoJSON(uri, tiles)
-          .pipe(json)
-      })
-  }
+  var tiles = process.argv.length <= 3 ? null : [process.argv.slice(3, 6).map(Number)]
+  vectorTilesToGeoJSON(uri, tiles).pipe(json)
 }
