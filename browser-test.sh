e2b795aea38cb50486c6efd7188eec77e8997cb4
Anand Thakker <vestibule@anandthakker.net>
2015-07-29 11:04:25 -0400

    Switch from testling to tape-run for travis

--- browser-test.sh
+++ browser-test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-node_modules/.bin/browserify -t brfs test.js | node_modules/.bin/testling
+node_modules/.bin/browserify -t brfs -t envify test.js | node_modules/.bin/tape-run --browser phantomjs
